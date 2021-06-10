e5a03871d727d685429e3ad17040b83fbe6d4569
zongyue <zw16hzy@126.com>
2021-06-10 18:21:35 +0100

    add if section

--- 3_operators.swift
+++ 3_operators.swift
@@ -1,23 +1,24 @@
 /* Operators
-  1. assignment operator
-  2. arithmetic operators
-  3. remainder operator
-  4. unary plus/minus operator
-  5. compound assignment operators
-  6. comparison operators
-  7. ternary conditional Operator
-  8. nil coalescing operator
-  9. range operators: a...b a..<b [..<2]
-  10. logical operators: ! && ||
-  11. bitwise operators
-  12. overflow operators
+   1. assignment operator
+   2. arithmetic operators
+   3. remainder operator
+   4. unary plus/minus operator
+   5. compound assignment operators
+   6. comparison operators
+   7. ternary conditional Operator
+   8. nil coalescing operator
+   9. range operators: a...b a..<b [..<2]
+   10. logical operators: ! && ||
+   11. bitwise operators
+   12. overflow operators
   
-  - precedence and associativity
-  - operator methods
-  - custom operators
-  - result builders
-
+   - precedence and associativity
+   - operator methods
+   - custom operators
+   - result builders
 */
+
+print("hello operators")
 
 // unary
 let three = 3
